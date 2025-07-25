2045d0fdde25be0c592925f241578d9b2d83b024
Robert Adoga <ukurowo@gmail.com>
2025-07-25 18:18:06 +0200

    Fix Tailwind CSS by using @tailwindcss/postcss in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/postcss'; 
+import autoprefixer from 'autoprefixer';
+import postcssNesting from 'postcss-nesting';
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer(), postcssNesting()],
+    },
+  },
+});
