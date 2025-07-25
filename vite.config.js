c05b74c911e9863d4e81e212f2910a4e1dc7bac5
Robert Adoga <ukurowo@gmail.com>
2025-07-25 17:08:22 +0200

    On branch main

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    react()
-  ],
-   build: {
+  plugins: [react()],
+  build: {
     outDir: 'dist'
   }
 })
